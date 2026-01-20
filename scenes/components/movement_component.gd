b70f9d1911373defc9a69fcb31716e4abb9b35f3
Matthew Muenzberg <matthew.muenzberg@gmail.com>
2026-01-19 21:08:50 -0500

    Made decel much better, increased window size and added sample level

--- scenes/components/movement_component.gd
+++ scenes/components/movement_component.gd
@@ -3,15 +3,15 @@ extends Node
 
 @export_subgroup("Settings")
 @export var speed: float = 300
-@export var ground_accel_speed: float = 200.0
-@export var ground_decel_speed: float = -400.0
-@export var air_accel_speed: float = 20.0
-@export var air_decel_speed: float = 6.0
+@export var ground_accel_speed: float = 50.0
+@export var ground_decel_speed: float = 45.0
+@export var air_accel_speed: float = 50.0
+@export var air_decel_speed: float = 45.0
 
 
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
 	var velocity_change_speed: float = 0.0
-	if direction:
+	if body.is_on_floor():
 		velocity_change_speed = ground_accel_speed if direction != 0 else ground_decel_speed
 	else:
 		velocity_change_speed = air_accel_speed if direction != 0 else air_decel_speed
