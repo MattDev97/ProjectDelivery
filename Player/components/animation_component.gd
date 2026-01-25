847f5531976fafa91ca2ec0f573e5ada878c40f1
Matthew Muenzberg <matthew.muenzberg@gmail.com>
2026-01-25 12:49:00 -0500

    Revert "Reverting"
    This reverts commit 1844413e3c5d68dc00c13ad36835c391e1ba0560.

--- Player/components/animation_component.gd
+++ Player/components/animation_component.gd
@@ -5,12 +5,6 @@ extends Node
 @export var sprite: AnimatedSprite2D
 @onready var body: CharacterBody2D = get_parent()
 
-func handle_horizontal_flip(move_direction: float) -> void:
-	if move_direction == 0:
-		return
-	 
-	sprite.flip_h = false if move_direction > 0 else true
-	
 func handle_move_animation(move_direction: float) -> void:
 	var body_scale = -1 if move_direction > 0 else 1
 	print('body_scale: ' + str(body_scale))
